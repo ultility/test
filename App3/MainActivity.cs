494489267733c648cece91fdd46ccbe78c985e56
StudentC <StudentC@CYBERN-M>
2021-11-02 09:56:26 +0200

    Add project files.

---
+++ App3/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int counter = 5;
+        Button btnPlus, btnMinus;
+        EditText et;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnPlus = (Button)FindViewById(Resource.Id.btnPlus);
+            btnMinus = (Button)FindViewById(Resource.Id.btnMinus);
+            et = (EditText)FindViewById(Resource.Id.et);
+
+            btnPlus.Click += BtnPlus_Click;
+
+        }
+
+        private void BtnPlus_Click(object sender, System.EventArgs e)
+        {
+            counter++;
+            et.Text = "" + counter;
+        }
+    }
+}
